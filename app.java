6a1a262d6b48e1c65291a5ee8a8c86386107577d
abdulraheemshaik144 <abdulraheemshaik144@gmail.com>
2025-11-13 11:46:08 +0530

    java code dev2 written

--- app.java
+++ app.java
@@ -1,3 +1,13 @@
 <h1>Welcome to git hub class</h1>
   <b1>Hi</b1>
   
+/dev 2
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 10; // Declare and initialize integer variable num1
+        int num2 = 20; // Declare and initialize integer variable num2
+        int sum = num1 + num2; // Calculate the sum
+        System.out.println("The sum is: " + sum); // Prints the sum
+    }
+}
+  
